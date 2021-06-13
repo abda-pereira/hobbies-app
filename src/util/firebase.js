bfe19d11be73f672564084d9e7b5bc7cda950a2f
Abda Pereira <abdapereira@hotmail.com>
2021-06-13 17:52:39 -0300

    Update firebase.js

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -3,14 +3,7 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAylOgf59toZm92xu19bKuaOGpoxVpwz54",
-  authDomain: "hobbies-app-aqedutech.firebaseapp.com",
-  databaseURL: "https://hobbies-app-aqedutech-default-rtdb.firebaseio.com",
-  projectId: "hobbies-app-aqedutech",
-  storageBucket: "hobbies-app-aqedutech.appspot.com",
-  messagingSenderId: "844234260684",
-  appId: "1:844234260684:web:3afc4e5caeede4169e0269",
-  measurementId: "G-RNCPJG5EBF"
+  
 };
 
 // Initialize Firebase
